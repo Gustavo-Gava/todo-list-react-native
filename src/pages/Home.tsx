80f93de0f314f0a60400fa00ded8af79133339f7
Gustavo Gava <gustavogava123@hotmail.com>
2021-09-24 11:15:44 -0300

    Funcionalides feitas: adicionar e remover tarefas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,23 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    console.log('oi')
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    // const newTasks = tasks.map(task => task.id === id ? task.done = !task.done : task)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newTasks = tasks.filter(task => task.id !== id)
+    setTasks(newTasks)
   }
 
   return (
